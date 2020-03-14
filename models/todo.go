a9dde2dbde26d931fcf910f8999e65feb28ab488
kankan <sunagakannta7@gmail.com>
2020-03-14 11:53:01 +0900

    feat:update追加

--- models/todo.go
+++ models/todo.go
@@ -7,18 +7,29 @@ import (
 )
 
 type ToDo struct{
-	id 	  int 	 `json:"id"`
+	Id 	  int 	 `json:"id"`
 	Title string `json:"title"`
 	Body  string `json:"body"`
 	Done  bool	 `json:"done"`
 }
 
-func NewTodo (todoPram []byte) ToDo {
+func JsonToTodoStruct (todoPram []byte) ToDo {
 	var  ToDo ToDo
 	json.Unmarshal(todoPram, &ToDo)
 	ToDo.Done = false
 	fmt.Printf("(%%+v) %+v\n", ToDo)
 	return ToDo
+}
+
+func (td *ToDo) Update() error {
+	query := fmt.Sprintf(`
+		UPDATE todos SET title = ?, body = ?, done = ? WHERE id = ?;
+	`)
+	_, err := Dbconecction.Exec(query, td.Title, td.Body, td.Done, td.Id)
+	if err != nil{
+		log.Println(err)
+	}
+	return err
 }
 
 func (td *ToDo) Save() error {
